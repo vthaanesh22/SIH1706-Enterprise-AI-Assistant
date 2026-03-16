f82736bc3f16ecfb47798fa55ba26563a6ae391f
Thaanesh <139373686+vthaanesh22@users.noreply.github.com>
2026-03-16 20:23:53 +0530

    Update email_otp.py

--- auth/email_otp.py
+++ auth/email_otp.py
@@ -1,1 +1,15 @@
+import random
 
+def send_otp():
+    otp = random.randint(100000,999999)
+    print("Your OTP:", otp)
+    return otp
+
+generated_otp = send_otp()
+
+user_otp = int(input("Enter OTP: "))
+
+if user_otp == generated_otp:
+    print("Login successful")
+else:
+    print("Invalid OTP")
