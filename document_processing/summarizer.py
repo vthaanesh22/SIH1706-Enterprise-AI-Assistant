1d93559dd6ee846fb8ceb6fd92f6310cfd9d0053
Thaanesh <139373686+vthaanesh22@users.noreply.github.com>
2026-03-16 20:23:27 +0530

    Update summarizer.py

--- document_processing/summarizer.py
+++ document_processing/summarizer.py
@@ -1,1 +1,12 @@
+from transformers import pipeline
 
+summarizer = pipeline("summarization")
+
+text = """
+Artificial Intelligence helps organizations automate tasks,
+improve efficiency, and assist employees with decision making.
+"""
+
+summary = summarizer(text, max_length=40, min_length=10, do_sample=False)
+
+print(summary[0]['summary_text'])
