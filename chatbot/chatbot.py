34ccf53af8c3c4d3635b8329dd8e2498f9774046
Thaanesh <139373686+vthaanesh22@users.noreply.github.com>
2026-03-16 20:23:02 +0530

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,1 +1,23 @@
+def chatbot():
+    print("Enterprise Assistant Chatbot")
+    
+    while True:
+        user = input("Employee: ").lower()
 
+        if "leave" in user:
+            print("Bot: Employees are allowed 12 casual leaves per year.")
+        
+        elif "password" in user:
+            print("Bot: Please reset your password through the IT portal.")
+        
+        elif "event" in user:
+            print("Bot: The annual company meeting will be held next month.")
+        
+        elif "exit" in user:
+            print("Bot: Goodbye!")
+            break
+        
+        else:
+            print("Bot: I will help you with HR or IT related queries.")
+
+chatbot()
